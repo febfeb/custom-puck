abdded17e821d33a101a893df56d0baa5a316b1e
Febrianto Arif Rakhman <febfeb.90@gmail.com>
2026-05-21 17:51:52 +0700

    fix: first commit

---
+++ apps/docs/pages/docs/integrating-puck/_meta.js
@@ -0,0 +1,17 @@
+const menu = {
+  "component-configuration": {},
+  "root-configuration": {},
+  "multi-column-layouts": {},
+  categories: {},
+  "rich-text-editing": {},
+  "dynamic-props": {},
+  "dynamic-fields": {},
+  "external-data-sources": {},
+  "server-components": {},
+  "data-migration": {},
+  styling: {},
+  viewports: {},
+  "feature-toggling": {},
+};
+
+export default menu;
